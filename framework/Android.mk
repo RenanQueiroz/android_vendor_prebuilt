a587480edeeb99f023a1f358f85d65dce48470a7
RenanQueiroz <queirozrrq@gmail.com>
2018-02-10 17:50:04 -0500

    Add GoogleCamera app and add Dialer support

---
+++ framework/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.camera.experimental2016
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := com.google.android.camera.experimental2016.jar
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.dialer.support
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := com.google.android.dialer.support.jar
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
